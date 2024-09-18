3736561e6f5a552d88562eac91be1a6e023ebc1b
Mafe-Garcia <mariaf.garcia@upb.edu.co>
2024-09-18 07:39:58 -0500

    lambert

---
+++ compGraf/Assets/shaders/includes/Lighting.hlsl
@@ -0,0 +1,17 @@
+
+//#include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/Lighting.hlsl"
+
+
+
+void GetMainLight_float(out float3 direction, out float3 color)
+{
+    #if !defined(SHADERGRAPH_PREVIEW)
+    Light light;
+    light = GetMainLight();
+    direction = light.direction;
+    color = light.color;
+    #else
+    direction= float3(1,1,-1);
+    color = 1;
+    #endif
+}
